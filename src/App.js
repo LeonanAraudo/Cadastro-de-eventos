9776bd6631910d6eb766556157ece536fc3aa21d
LeonanAraudo <LeonanArajoReis@outlook.com>
2023-11-25 22:12:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <p className="com">Compartilhe seu evento</p>
+
+      <div className="area">
+       <p><i class="fa-regular fa-image"></i>Escolher Imagem</p>
+      </div>
+       <div className="form">
+
+      <div className=" título">
+        <label>Titulo do evento</label>
+         <input type="text" placeholder="Digite o título do evento" required></input>
+      </div>
+
+      <div className=" data">
+        <label>Data do evento</label>
+        <input type="text" placeholder="Digite a data do evento" required></input>
+      </div>
+
+      <div className="descricao">
+        <label>Descrição do evento</label>
+        <textarea  placeholder="Digite a descrição do evento" rows="4" required></textarea>
+      </div>
+
+      <div className="categorias">
+      <select name="" id="Categoria" required>
+                <optgroup>
+                <option id="lace" disabled selcteed hidden>Categoria do evento</option>
+                </optgroup>
+                <option className="option" value="cater ">carter </option>
+                <option className="option" value="cater">carter</option>
+                <option className="option" value="cater">carter</option>
+              </select>
+      </div>
+
+      <div className=" Local">
+       <label>Localização do evento</label>
+       <input type="text" placeholder="Digite a localização do evento"></input>
+      </div>
+
+      <div className="bot">
+      <button className="botao">Publicar</button>
+      </div>
+      </div>
     </div>
   );
 }
